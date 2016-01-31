22ec372dfa7807e670ed1d46459f62ecceb8ae54
Ethan Godt <ethan@ethangodt.com>
2016-01-30 18:28:09 -0800

    Registered homepage view with UI router

--- client/scripts/paypal-app.js
+++ client/scripts/paypal-app.js
@@ -2,6 +2,45 @@
   'use strict';
 
   angular
-    .module('paypal-app', [])
+    .module('paypal-app', [
+      'ui.router'
+    ])
+    .config(router);
+
+  router.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function router($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        views: {
+          header: {
+            templateUrl: 'scripts/homepage/header.tmpl.html'
+          },
+          content: {
+            templateUrl: 'scripts/homepage/content.tmpl.html'
+          }
+        }
+      })
+      .state('send-money', {
+        url: '/send-money',
+        views: {
+          header: {
+          },
+          content: {
+          }
+        }
+      })
+      .state('transactions', {
+        url: '/transactions',
+        views: {
+          header: {
+          },
+          content: {
+          }
+        }
+      })
+  }
 
 }());
